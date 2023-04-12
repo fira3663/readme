45efb5884be7becab94eac4cc7fdf9bd020dd522
fira3663 <meler999@mail.ru>
2023-04-13 00:40:13 +0300

    add style for site

--- task1.js
+++ task1.js
@@ -1,1 +1,12 @@
-console.log(1)
+function makeFunc() {
+    var name = "Mozilla";
+  
+    function displayName() {
+      console.log(name);
+    }
+  
+    return displayName;
+  };
+  
+  var myFunc = makeFunc();
+  myFunc();
